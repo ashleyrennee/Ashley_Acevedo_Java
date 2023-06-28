f52431b2dc964054432d3f95f5545223f9e5a291
Ashley Acevedo <40500769+ashleyrennee@users.noreply.github.com>
2023-06-27 21:01:27 -0400

    Changed repos

---
+++ M2-Challenge-Acevedo-Ashley/src/main/java/com/company/chatterbook/User.java
@@ -0,0 +1,24 @@
+package com.company.chatterbook;
+import java.util.ArrayList;
+import java.util.List;
+public class User {
+    public String name;
+    List<ChatterPost> chatterPosts = new ArrayList<>();
+    public User(String name){
+        this.name = name;
+    }
+    public List<ChatterPost> getChatterPosts(){
+        return chatterPosts;
+    }
+    public void setChatterPosts(List<ChatterPost> chatterPosts) {
+        this.chatterPosts = chatterPosts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
